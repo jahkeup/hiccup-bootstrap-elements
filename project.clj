d222652ab0fdfb66e61986a7940c2ac8458a2d9e
Jacob Vallejo <you@jahkeup.com>
2014-04-04 12:34:32 -0400

    Touch release, 3.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hiccup-bootstrap-elements "3.1.1-SNAPSHOT"
+(defproject hiccup-bootstrap-elements "3.1.1"
   :description "Common Bootstrap 3 Elements for Hiccup"
   :url "http://github.com/jahkeup/hiccup-bootstrap-elements"
   :license {:name "Eclipse Public License"
